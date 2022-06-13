8932178ac012bd5c61344f30dca8a079dea10f45
DarioMendoza <dm1289@gmail.com>
2022-06-13 14:03:30 -0600

    Added database elements controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpensesController } from './controllers/expenses/expenses/expenses.controller';
+import { CategoriesController } from './controllers/categories/categories/categories.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ExpensesController],
+  controllers: [AppController, ExpensesController, CategoriesController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/categories/categories/categories.controller.ts
+++ src/controllers/categories/categories/categories.controller.ts
@@ -1,18 +1,16 @@
 import { Controller, Get, Post, Res, HttpStatus, Body } from '@nestjs/common';
 import { Response } from 'express';
 
-@Controller('expenses')
-export class ExpensesController {
-
+@Controller('categories')
+export class CategoriesController {
     @Post()
     create(@Body() body: any, @Res() res: Response){
-
-        res.status(HttpStatus.CREATED).send('Expense added');
+        res.status(HttpStatus.CREATED).send('Category added added');
     }
 
     @Get()
     findAll(@Res() res: Response){
         
-        res.status(HttpStatus.OK).json('List of all expenses');
+        res.status(HttpStatus.OK).json('List of all categories');
     }
 }
